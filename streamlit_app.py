3d6d9a0c3d11d06df07525e5a76613c5674f46cd
jitinvig <84938238+jitinvig@users.noreply.github.com>
2023-04-01 23:03:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
 streamlit.body('Omega 3 & blueberry oatmeal')
-streamlit.body('Kale, Spinach & Rocket Smoothie')  
-streamlit.body('Hard Boiled Free-Range Egg')
+
