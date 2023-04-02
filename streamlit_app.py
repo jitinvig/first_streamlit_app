c26722b8051b3f6e37a00c593c4a73bfc001b7da
jitinvig <84938238+jitinvig@users.noreply.github.com>
2023-04-01 22:59:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
-streamlit.body('Omega 3 & blueberry oatmeal'
-               'Kale, Spinach & Rocket Smoothie'  
-              'Hard Boiled Free-Range Egg')
+streamlit.body('Omega 3 & blueberry oatmeal')
+streamlit.body('Kale, Spinach & Rocket Smoothie')  
+streamlit.body('Hard Boiled Free-Range Egg')
